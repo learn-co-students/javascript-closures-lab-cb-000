111b6feaa349dc2ba79f595a8297c318efbf6908
maxgrok <maxgrok2011@gmail.com>
2016-12-12 18:59:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 const app = "I don't do much."
+
+function bumpCounter(){
+ 	var counter = 0; 
+	
+	function addBump(){ 
+		counter = counter + 1;
+		return counter;
+	}
+
+	function getBumps(){ 
+		return counter;
+	}
+
+	return {
+		addBump,
+		getBumps
+		};
+}
+
+function createAnimal(animalType){ 
+	function deadlyDevice(weapon){ 
+		var dangerousAnimals = {animalType:animalType, deadlyDevice:weapon};
+		return dangerousAnimals;
+	}
+	return deadlyDevice;
+}
+
+const sharkCreator = createAnimal('Shark');
+const sharkWithFrickinLaserbeam = sharkCreator('Laserbeam'); 
+const sharkWithFrickinCannon = sharkCreator('Cannon');

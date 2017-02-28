d977d98f865af487768a3b27964c127ff93fd114
Juha  Bach <cdalsoniii@gmail.com>
2017-02-28 11:53:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 const app = "I don't do much."
+
+
+function bumpCounter () {
+  var counter = 0;
+  var addBump =  function () {counter ++};
+  var getBumps = function () {return counter};
+  return {addBump,getBumps};
+}
+function createAnimal(animalType) {
+  return function (deadlyDevice) {
+    return {
+      deadlyDevice,
+      animalType
+    }
+  }
+}
+
+var sharkCreator = createAnimal('Shark');
+var sharkWithFrickinLaserbeam = sharkCreator ('Laserbeam');
+var sharkWithFrickinCannon = sharkCreator ('Cannon');

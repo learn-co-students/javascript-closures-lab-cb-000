0d7a0e19dbd369022e36c80c931c2e6314086a7e
Josef Butts <jbutts@utexas.edu>
2017-06-08 04:00:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-const app = "I don't do much."
+//const app = "I don't do much.";
+
+function bumpCounter(){
+  let counter = 0;
+   function addBump(){ //adds one to the counter
+    counter++;
+
+  }
+   function getBumps(){
+    return counter;
+  }
+  return {
+    addBump, getBumps,
+  };
+}
+
+//Dangerous animals
+function createAnimal(animalType){
+  return function (deadlyDevice){
+    return {animalType, deadlyDevice};
+
+  }
+}
+var sharkCreator = createAnimal('Shark');
+var sharkWithFrickinLaserbeam = sharkCreator( 'Laserbeam');
+var sharkWithFrickinCannon = sharkCreator('Cannon');

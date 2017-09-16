034e8078b53fffdd7af8835f960dc7fa0698f261
Tyler Bristol <tahbristol@gmail.com>
2017-09-16 03:49:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0;
+
+  function addBump() {
+    counter++;
+  }
+
+  function getBumps() {
+    return counter;
+  }
+
+  return {
+    addBump,
+    getBumps
+  };
+}
+
+function createAnimal(animalType) {
+
+
+  return (deadlyDevice) => {
+    return {
+      animalType: animalType,
+      deadlyDevice: deadlyDevice
+    };
+  };
+
+}
+
+var sharkCreator = createAnimal("Shark");
+var sharkWithFrickinLaserbeam = createAnimal("Shark")("Laserbeam");
+var sharkWithFrickinCannon = createAnimal("Shark")("Cannon");

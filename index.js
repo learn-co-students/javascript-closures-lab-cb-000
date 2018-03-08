03fbdcaa9993fbd689550260e76ff8cbe0aeab54
Alex Segers <a.segers.dev@gmail.com>
2018-03-08 01:58:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var bumps = 0
+  function addBump() { bumps += 1; }
+  function getBumps() { return bumps }
+
+  return {addBump, getBumps}
+}
+
+function createAnimal(animalType) {
+  function device(deadlyDevice) {
+    return { animalType: animalType, deadlyDevice: deadlyDevice }
+  }
+
+  return {device}
+
+}
+
+function sharkCreator() {
+  return createAnimal('Shark')
+}
+
+var sharkWithFrickinLaserbeam = {animalType: "Shark", deadlyDevice: "Laserbeam"}
+var sharkWithFrickinCannon = {animalType: "Shark", deadlyDevice: "Cannon"}

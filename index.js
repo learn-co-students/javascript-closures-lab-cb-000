d6f01610520d2f5279fc4c46783d002bac7c5844
theboymo <billfero@gmail.com>
2018-03-24 11:16:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-const app = "I don't do much."
+// solutions
+"use strict";
+
+function bumpCounter() {
+  let counter = 0
+
+  let addBump = ()=>{
+    counter++
+  }
+
+  let getBumps = ()=>{
+    return counter;
+  }
+
+
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+
+function createAnimal(animalType) {
+
+  return (deadlyDevice)=>{
+    return {animalType: animalType, deadlyDevice: deadlyDevice}
+  }
+}
+
+var sharkCreator = createAnimal('Shark')
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')

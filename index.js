fc156f03cb9954d7bbdead798172eebeef06cc64
Chris Nunez <cenunez47@gmail.com>
2018-04-08 15:46:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+
+function bumpCounter(callback) {
+  var counter = 0;
+
+  function addBump() {
+    return counter++;
+  }
+
+  function getBumps() {
+    return counter;
+  }
+
+  return {
+    addBump,
+    getBumps
+  };
+}
+
+function createAnimal(animalType) {
+ 
+  return function (deadlyDevice) {
+    return { animalType, deadlyDevice };
+  }
+  
+}
+
+const sharkCreator = createAnimal('Shark');
+const sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+const sharkWithFrickinCannon = sharkCreator('Cannon');

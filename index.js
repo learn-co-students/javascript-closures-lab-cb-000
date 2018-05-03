8db7b0a64220bf87ed755627087154eafe3bec82
Patrick J Louis <lumenfuzz@gmail.com>
2018-05-03 11:30:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0
+  function addBump() {
+    counter = counter + 1
+  }
+  function getBumps() {
+    return counter
+  }
+  return { addBump, getBumps }
+}
+
+function createAnimal(animalType) {
+  function diabolicalCreation(deadlyDevice) {
+    return { animalType, deadlyDevice }
+  }
+  /* var sharkWithFrickinLaserbeam = diabolicalCreation('Shark','Laserbeam')
+  var sharkWithFrickinCannon = diabolicalCreation('Shark','Cannon') */
+  return diabolicalCreation
+}
+
+var sharkCreator = createAnimal('Shark')
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')

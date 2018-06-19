41ff48120472fa8564675d736d2561cfadce4ed0
Tao <werewaswas@live.com>
2018-06-19 08:25:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  let counter = 0;
+
+  function addBump() {
+    counter = counter + 1;
+  }
+
+  function getBumps() {
+    return counter;
+  }
+
+  return { addBump, getBumps };
+}

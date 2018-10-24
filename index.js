2e0cb63a99dcf44f26337e211f82a52d35646a24
Pearl Zeng <zhuzeng0418@gmail.com>
2018-10-24 15:12:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  let counter = 0
+  
+  function addBump() {
+    counter++
+    return counter
+  }
+  
+  function getBumps() {
+    return counter 
+  }
+  
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+function createAnimal(animalType) {
+  function createAnimal(deadlyDevice) {
+    return {
+      animalType: animalType,
+      deadlyDevice: deadlyDevice
+    }
+  }
+  
+  return createAnimal
+}
+
+const sharkCreator = createAnimal('Shark')
+
+const sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+
+const sharkWithFrickinCannon = sharkCreator('Cannon')

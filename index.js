84ec00a8837a1c23bd84b9891e6ca5d7bac9ffa3
marysue <mary@marylark.com>
2019-01-09 12:15:30 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+    var counter = 0;
+
+    function addBump() {
+        counter++;
+    }
+
+    function getBumps() {
+        return counter;
+    }
+    return { addBump, getBumps }
+}
+
+function createAnimal(animalType) {
+    function deadlyDevice (deadlyDevice) {
+        newObj = new Object( {animalType: animalType, deadlyDevice: deadlyDevice});
+        return newObj;
+    }
+
+    
+ 
+
+    return deadlyDevice;
+}
+
+var sharkCreator =  createAnimal('Shark');
+var sharkWithFrickinLaserbeam = new Object( {animalType: 'Shark', deadlyDevice: 'Laserbeam'} )
+var sharkWithFrickinCannon = new Object ( {animalType: 'Shark', deadlyDevice: 'Cannon'});

ae5d8e3dead13aca0ecfdc76a26e379a5bcf9f47
Chris Franklin <cf73021@gmail.com>
2019-01-31 17:49:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-const app = "I don't do much."
+function bumpCounter(){
+  var counter = 0
+  function addBump(){
+    counter += 1
+  }
+  function getBumps(){
+    return counter
+  }
+  return {addBump, getBumps}
+}
+
+function createAnimal(animalType){
+  function addDevice(deadlyDevice){
+    var creation = {animalType: animalType, deadlyDevice: deadlyDevice}
+    return creation
+  }
+  return addDevice
+}
+
+var sharkCreator = createAnimal('Shark')
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')

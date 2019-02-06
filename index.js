3e282e0da44b9f218a811245b7d98020c5f36c11
Joe Corwin <joe.corwin@gmail.com>
2019-02-06 23:22:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-const app = "I don't do much."
+function bumpCounter() {
+  var counter = 0
+  function addBump() {
+    counter++
+  };
+
+  function getBumps() {
+    return counter
+
+  }
+
+  return {
+    addBump,
+    getBumps
+  }
+}
+
+
+function createAnimal(animalType) {
+
+   function dangerousAnimals(deadlyDevice) {
+    return {
+      animalType: animalType,
+      deadlyDevice: deadlyDevice
+    }
+}
+return dangerousAnimals
+}
+
+var sharkCreator = createAnimal("Shark");
+var sharkWithFrickinLaserbeam = sharkCreator("Laserbeam");
+var sharkWithFrickinCannon = sharkCreator("Cannon")

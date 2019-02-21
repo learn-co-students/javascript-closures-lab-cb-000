6bb8dcbc9a837c9a7ecc3760e5cc84ffcd68e388
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-21 20:50:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 const app = "I don't do much."
+function bumpCounter() {
+  var counter = 0 ;
+  function addBump() {
+    counter += 1
+  }
+  function getBumps() {
+    return counter
+  }
+  return {addBump, getBumps}
+}
+/* var {addBump, getBumps} =   bumpCounter() */
+function createAnimal(animalType) {
+  function combine(deadlyDevice) {
+    return{'animalType': animalType, 'deadlyDevice': deadlyDevice}
+  }
+  return combine
+}
+
+var sharkCreator = createAnimal('Shark')
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam')
+var sharkWithFrickinCannon = sharkCreator('Cannon')

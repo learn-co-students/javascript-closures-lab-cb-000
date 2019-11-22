5794733bce6c3200940086668137a2a7a5334b2c
Will Hodgson <will.hodgson@current-rms.com>
2019-11-22 14:43:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-const app = "I don't do much."
+function bumpCounter() {
+  let counter = 0;
+
+  function addBump() {
+    counter++;
+  }
+
+  function getBumps() {
+    return counter
+  }
+
+  return {
+    addBump,
+    getBumps,
+  };
+}
+
+
+function createAnimal(animalType) {
+  return function (deadlyDevice) {
+    return {
+      animalType: animalType,
+      deadlyDevice: deadlyDevice
+    }
+  };
+}
+
+var sharkCreator = createAnimal('Shark');
+
+var sharkWithFrickinCannon = sharkCreator('Cannon');
+var sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
